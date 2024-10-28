26df19d71d323db6aa796cf77158f2412b356027
domonkosgyomorey <domonkosgyomorey@gmail.com>
2024-10-28 15:50:12 +0100

    folder added for the model metrics

--- model_trainer/generate_model_metrics.py
+++ model_trainer/generate_model_metrics.py
@@ -1,13 +1,19 @@
-from os import listdir
-from os.path import isfile, join
+import os
 import datetime
-
 from keras import models
+
 from my_cnn_models.cnn_util import validate_models
 
 prefix = '../models/'
 file_names = ['category_class_v4.model.keras', 'digit_class_v4.model.keras', 'operator_class_v4.model.keras', 'paren_class_v4.model.keras']
 datasets = ['dataset', 'dataset/digit', 'dataset/operator', 'dataset/paren']
+model_metric_logs_path = 'model_metric_logs'
+os.makedirs(os.path.dirname(model_metric_logs_path), exist_ok=True)
 
 for model_name, ds_path in zip(file_names, datasets):
-    validate_models(models.load_model(prefix+model_name), ds_path, model_name+'_metrics_'+str(datetime.datetime.now().date()).replace('-', '_')+'.xlsx')
+    validate_models(
+        models.load_model(prefix+model_name),
+        ds_path,
+        model_metric_logs_path+model_name+
+        '_metrics_'+str(datetime.datetime.now().date()).replace('-', '_')+'.xlsx'
+    )
